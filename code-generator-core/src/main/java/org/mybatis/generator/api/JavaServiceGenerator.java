9867d06b28c93069b94fc1c7a90ff441f5274f4c
tang <tangextr@gmail.com>
2017-10-13 17:07:52 +0800

    first commit

---
+++ code-generator-core/src/main/java/org/mybatis/generator/api/JavaServiceGenerator.java
@@ -0,0 +1,137 @@
+/**
+ *    Copyright ${license.git.copyrightYears} the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.generator.api;
+
+import freemarker.template.Template;
+import org.mybatis.generator.config.*;
+import org.mybatis.generator.internal.util.JavaBeansUtil;
+import org.mybatis.generator.internal.util.StringUtility;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaServiceGenerator {
+
+    private Logger logger = LoggerFactory.getLogger(JavaServiceGenerator.class);
+
+    private Configuration configuration;
+
+    JavaServiceGenerator(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    private List<JavaServiceTemplate> generateTemplateDataList() {
+        List<Context> contexts = configuration.getContexts();
+        List<JavaServiceTemplate> templateList = new ArrayList<JavaServiceTemplate>();
+        String curPath = System.getProperty("user.dir");
+        for (Context ctx : contexts){
+            JavaServiceGeneratorConfiguration jgc = ctx.getJavaServiceGeneratorConfiguration();
+            String templatePath = ctx.getJavaServiceGeneratorConfiguration().getTemplatePath();
+            List<TableConfiguration> tableConfigurations = ctx.getTableConfigurations();
+            for (TableConfiguration table : tableConfigurations) {
+
+                logger.info(JavaBeansUtil.getCamelCaseString(table.getGeneratedKey().getColumn(), false));
+                String tbName = table.getDomainObjectName();
+                JavaServiceTemplate template = new JavaServiceTemplate();
+
+                if (templatePath != null && !templatePath.isEmpty()) {
+                    template.setTemplatePath(templatePath);
+                } else {
+                    template.setTemplatePath("default:/template");
+                }
+
+                if (table.getGeneratedKey() != null) {
+                    template.setPrimaryKeyName(JavaBeansUtil.getCamelCaseString(table.getGeneratedKey().getColumn(), false));
+                    template.setPrimaryKeyType(table.getGeneratedKey().getType());
+                }
+                template.setInterfaceName(tbName + "Service");
+                template.setServiceName(tbName + "ServiceImpl");
+                template.setMapperClzName(tbName + "Mapper");
+                template.setMapperDefineName(lowerCamelCase(tbName) + "Mapper");
+                template.setModelClzName(tbName);
+                template.setModelDefineName(lowerCamelCase(tbName));
+                template.setModelPackage(ctx.getJavaModelGeneratorConfiguration().getTargetPackage());
+                template.setMapperPackage(ctx.getJavaClientGeneratorConfiguration().getTargetPackage());
+                String servicePath = jgc.getTargetProject()+"/"+jgc.getTargetPackage().replaceAll("\\.","/")+"/";
+                template.setServicePath(servicePath);
+                template.setProjectPath(curPath);
+                template.setServicePackage(jgc.getTargetPackage());
+                templateList.add(template);
+            }
+        }
+        return templateList;
+    }
+
+    private String lowerCamelCase(String source) {
+
+        return source.isEmpty() ? "" :
+                Character.toLowerCase(source.charAt(0)) + source.substring(1);
+    }
+
+    /**
+     * 通过freemarker生成接口和servcie实现
+     */
+    public void generate() {
+        List<JavaServiceTemplate> templateList = generateTemplateDataList();
+        freemarker.template.Configuration cfg = new freemarker.template.Configuration(freemarker.template.Configuration.getVersion());
+        try  {
+            String templatePath = "";
+            for (JavaServiceTemplate template : templateList) {
+                templatePath = template.getTemplatePath();
+                if (templatePath.indexOf("default:") == 0) {
+                    cfg.setClassForTemplateLoading(JavaServiceGenerator.class, templatePath.split(":")[1]);
+                } else if (templatePath.indexOf(".") == 0) {
+                    cfg.setDirectoryForTemplateLoading(new File(template.getProjectPath() + File.separator + templatePath.split(".")[1]));
+                } else {
+                    cfg.setDirectoryForTemplateLoading(new File(templatePath));
+
+                }
+                Template interfaceTmp = cfg.getTemplate("interfaceTemplate.ftl");
+
+                String serviceDirPath = template.getProjectPath() + File.separator + template.getServicePath();
+
+                File fileDir = new File(serviceDirPath);
+                if (!fileDir.exists()) {
+                    fileDir.mkdirs();
+                }
+
+                File interfaceFile = new File( serviceDirPath + File.separator + template.getInterfaceName() + ".java");
+
+                FileWriter interfaceFw = new FileWriter(interfaceFile);
+
+                interfaceTmp.process(template, interfaceFw);
+
+                Template serviceTmp = cfg.getTemplate("serviceTemplate.ftl");
+                serviceDirPath += File.separator + "impl";
+                File implDir = new File(serviceDirPath);
+                if (!implDir.exists()) {
+                    implDir.mkdirs();
+                }
+                File serviceFile = new File(serviceDirPath + File.separator + template.getServiceName() + ".java");
+                FileWriter serviceFw = new FileWriter(serviceFile);
+                serviceTmp.process(template, serviceFw);
+
+            }
+
+        } catch (Exception e) {
+            logger.error("service generate error", e);
+        }
+    }
+}
---
+++ code-generator-core/src/main/java/org/mybatis/generator/config/JavaServiceTemplate.java
@@ -0,0 +1,154 @@
+/**
+ *    Copyright ${license.git.copyrightYears} the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.generator.config;
+
+public class JavaServiceTemplate {
+    /** 项目所在路径 */
+    private String projectPath;
+    /** 自定义模版文件路径 */
+    private String templatePath;
+    /** 包名（接口所在包，实现类将自动放置在impl下 */
+    private String servicePackage;
+    /** model类 import 名 */
+    private String modelPackage;
+    private String mapperPackage;
+    private String interfaceName;
+    private String serviceName;
+    private String mapperClzName;
+    /** mapper变量名 */
+    private String mapperDefineName;
+    private String modelClzName;
+    /** model变量名 */
+    private String modelDefineName;
+    /** service 文件夹相对路径 */
+    private String servicePath;
+    /** 主键在model的命名 */
+    private String primaryKeyName;
+    /** 主键的类型 */
+    private String primaryKeyType;
+
+    public String getProjectPath() {
+        return projectPath;
+    }
+
+    public void setProjectPath(String projectPath) {
+        this.projectPath = projectPath;
+    }
+
+    public String getTemplatePath() {
+        return templatePath;
+    }
+
+    public void setTemplatePath(String templatePath) {
+        this.templatePath = templatePath;
+    }
+
+    public String getServicePackage() {
+        return servicePackage;
+    }
+
+    public void setServicePackage(String servicePackage) {
+        this.servicePackage = servicePackage;
+    }
+
+    public String getModelPackage() {
+        return modelPackage;
+    }
+
+    public void setModelPackage(String modelPackage) {
+        this.modelPackage = modelPackage;
+    }
+
+    public String getMapperPackage() {
+        return mapperPackage;
+    }
+
+    public void setMapperPackage(String mapperPackage) {
+        this.mapperPackage = mapperPackage;
+    }
+
+    public String getInterfaceName() {
+        return interfaceName;
+    }
+
+    public void setInterfaceName(String interfaceName) {
+        this.interfaceName = interfaceName;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getMapperClzName() {
+        return mapperClzName;
+    }
+
+    public void setMapperClzName(String mapperClzName) {
+        this.mapperClzName = mapperClzName;
+    }
+
+    public String getMapperDefineName() {
+        return mapperDefineName;
+    }
+
+    public void setMapperDefineName(String mapperDefineName) {
+        this.mapperDefineName = mapperDefineName;
+    }
+
+    public String getModelClzName() {
+        return modelClzName;
+    }
+
+    public void setModelClzName(String modelClzName) {
+        this.modelClzName = modelClzName;
+    }
+
+    public String getModelDefineName() {
+        return modelDefineName;
+    }
+
+    public void setModelDefineName(String modelDefineName) {
+        this.modelDefineName = modelDefineName;
+    }
+
+    public String getServicePath() {
+        return servicePath;
+    }
+
+    public void setServicePath(String servicePath) {
+        this.servicePath = servicePath;
+    }
+
+    public String getPrimaryKeyName() {
+        return primaryKeyName;
+    }
+
+    public void setPrimaryKeyName(String primaryKeyName) {
+        this.primaryKeyName = primaryKeyName;
+    }
+
+    public String getPrimaryKeyType() {
+        return primaryKeyType;
+    }
+
+    public void setPrimaryKeyType(String primaryKeyType) {
+        this.primaryKeyType = primaryKeyType;
+    }
+}
